02a9464d987c6f2a72b78d66e594774ae65f0d4d
Michael Chung <mikechung@live.com>
2019-03-19 15:25:17 -0700

    add scenarios 1-3

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            int sum;
+            if (String.IsNullOrEmpty(numbers))
+            {
+                sum = 0;
+            }
+            else
+            {
+                String[] stringArr = SplitNumbers(numbers);
+                int[] intArr = Array.ConvertAll(stringArr, int.Parse);
+                sum = intArr.Sum();
+            }
+            return sum;
+        }
+
+        private string[] SplitNumbers(string numbers)
+        {
+            string pattern = "[\n,]";
+            String[] elements = Regex.Split(numbers, pattern);
+            return elements;
+        }
+    }
+}
---
+++ StringCalculatorTest/CalculatorTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator;
+
+namespace StringCalculatorTest
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void When_StringIsEmpty_Expect_0()
+        {
+            string numbers = "";
+            int expected = 0;
+            Calculator test = new Calculator();
+            int actual = test.Add(numbers);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void When_StringIsNumber_Expect_NumberAsInt()
+        {
+            string numbers = "1";
+            int expected = 1;
+            Calculator test = new Calculator();
+            int actual = test.Add(numbers);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void When_StringIsMultipleNumbers_Expect_SumAsInt()
+        {
+            string numbers = "1,2";
+            int expected = 3;
+            Calculator test = new Calculator();
+            int actual = test.Add(numbers);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void When_DelimiterIsNewLineOrComma_Expect_SumAsInt()
+        {
+            string numbers = "1\n2,3";
+            int expected = 6;
+            Calculator test = new Calculator();
+            int actual = test.Add(numbers);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
